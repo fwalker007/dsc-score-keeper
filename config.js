8a204931dae499c16e75b482e5e3a3496c8a546e
Francine Walker <nospam@survivorsoft.com>
2022-02-07 15:31:18 +0000

    Changes to Get tailwind working !

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 
-  export const nftmanageraddress = "0x536F42766C793a410dD9290Bf116d6b7DA65649f"
-  export const nftaddress = "0xE9412467A7cB0DeABD24C2044758Ffa945f87bd3"
+  export const nftmanageraddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+  export const nftaddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
   
--- pages/_app.js
+++ pages/_app.js
@@ -1,67 +1,16 @@
-//import '../styles/globals.css'
+import '../styles/globals.css'
 import React from "react";
 import Table from "./Table"
 import WebAuthLoader from "./Web3Auth/WebAuthLoader.js"
 import QueryLoader from "./Web3Auth/QueryLoader.js"
 import Web3MinLoader from "./Web3Auth/WebMinLoader.js"
 import Web3AuthLogin from "./components/Web3AuthoLogin"
+import athletesData from "./TestData.json"
 
  
-const getData = () => [
-  {
-    name: "Jane Cooper",
-    meet1: "90",
-    meet2:  "120",
-    meet3:  "121",
-    imgUrl:
-      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-  },
-  {
-    name: "Cody Fisher",
-    meet1: "90",
-    meet2:  "120",
-    meet3:  "121",
-    imgUrl:
-      "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-  },
-  {
-    name: "Esther Howard",
-    meet1: "110",
-    meet2:  "112",
-    meet3:  "90",
-    imgUrl:
-      "https://images.unsplash.com/photo-1520813792240-56fc4a3765a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-  },
-  {
-    name: "Jenny Wilson",
-    meet1: "93",
-    meet2:  "80",
-    meet3:  "81",
-    imgUrl:
-      "https://images.unsplash.com/photo-1498551172505-8ee7ad69f235?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-  },
-  {
-    name: "Kristin Watson",
-    meet1: "122",
-    meet2:  "121",
-    meet3:  "122",
-    imgUrl:
-      "https://images.unsplash.com/photo-1532417344469-368f9ae6d187?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-  },
-  {
-    name: "Cameron Williamson",
-    meet1: "87",
-    meet2:  "117",
-    meet3:  "111",
-    imgUrl:
-      "https://images.unsplash.com/photo-1566492031773-4f4e44671857?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-  },
-];
-
-
 function Marketplace({ Component, pageProps }) 
 {
-  const data = React.useMemo(() => getData(), []);
+ // const data = React.useMemo(() => getData(), []);
 
   return(
 
@@ -76,7 +25,7 @@ function Marketplace({ Component, pageProps })
       </nav>
 
       <div >
-          <Table data={data} />
+          <Table data={athletesData} />
       </div>
 
       <Component {...pageProps} />
